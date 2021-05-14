43d341b2ed0bcdff1fa976048414bf36185dc06f
rab <r.ahmedbuehler@gmail.com>
2021-05-13 21:08:37 -0700

    Complete switch to boost/asio

--- stash_client.cpp
+++ stash_client.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <filesystem>
 #include <boost/asio.hpp>
 
-// g++ -I ~/HomeExt/boost_1_76_0 stash_client.cpp -std=c++17 -pthread
+// g++ -I ~/HomeExt/boost_1_76_0 stash_client.cpp -o client.out -std=c++17 -pthread
 
 class Stash_Client
 {
     private:
         std::string m_server {boost::asio::ip::host_name()};
-        const char* m_port {"3490"};
+        std::string m_port {"3490"};
         const std::filesystem::path m_stash_path {"Stash"};
 
         boost::asio::ip::tcp::socket connect()
         {
             boost::asio::io_context io_context;
-            boost::asio::ip::tcp::resolver resolver(io_context);
-            boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(m_server, m_port);
-            boost::asio::ip::tcp::socket socket(io_context);
-            boost::asio::connect(socket, endpoints);
+            boost::asio::ip::tcp::resolver resolver(io_context); //Associate <resolver> with <io_context>
+            boost::asio::ip::tcp::resolver::results_type endpoints {resolver.resolve(m_server, m_port)}; //Get endpoints
+            boost::asio::ip::tcp::socket socket(io_context); //Create socket and associate with <io_context>
+            boost::asio::connect(socket, endpoints); // Connect <socket> and an acceptable <endpoint>; automatically loops through <endpoints>
             return socket;
         }
 
@@ -39,15 +40,21 @@ class Stash_Client
 
             for (;;)
             {
-                char buf [128];
+                std::vector <char> data(128);
                 boost::system::error_code error;
 
-                size_t len = socket.read_some(boost::asio::buffer(buf), error);
+                size_t len = socket.read_some(boost::asio::buffer(data), error);
                 if (error == boost::asio::error::eof)
                     break; // Connection closed cleanly by peer.
                 else if (error)
                     throw boost::system::system_error(error); // Some other error.
-
+                else
+                {
+                    std::cout << "Received: ";
+                    for (int i {0}; i < data.size(); i++)
+                        std::cout << data[i];
+                    std::cout << "\n";
+                }
             }
 
             std::cout << "finished.\n";
@@ -75,9 +82,13 @@ int main(int argc, char* argv [])
     {
         std::cerr << e.what() << std::endl;
     }
+    catch (const char* e)
+    {
+        std::cerr << "\n" << e << std::endl;
+    }
     catch (...)
     {
-        std::cerr << "stash_client: an unexpected exception occurred.\n";
+        std::cerr << "\nStash_client: an unexpected exception occurred.\n";
     }
     return 0;
 }
--- stash_server.cpp
+++ stash_server.cpp
@@ -1,193 +1,52 @@
 #include <iostream>
-#include <cstdio> //perror
-#include <cstdlib> //perror, exit
-#include <unistd.h> //fork
-#include <cerrno> //errno
-#include <cstring>
-#include <sys/types.h> //waitpid, fork
-#include <sys/socket.h> //sockaddr
-#include <netinet/in.h> //sockaddr_in
-#include <netdb.h> //addrinfo
-#include <arpa/inet.h> //in_addr
-#include <sys/wait.h> //waitpid
-#include <csignal> //sigaction, all signal names
+#include <string>
+#include <boost/asio.hpp>
 
-// Handler for child process termination signal
-void sigchld_handler(int s)
+// g++ -I ~/HomeExt/boost_1_76_0 stash_server.cpp -o server.out -std=c++17 -pthread
+
+class Stash_Server
 {
-    // waitpid() might overwrite errno, so we save and restore it:
-    int saved_errno = errno;
+    private:
+        int m_port;
+        boost::asio::io_context m_io_context;
+        boost::asio::ip::tcp::acceptor m_acceptor;
 
-    while(waitpid(-1, nullptr, WNOHANG) > 0);
-
-    errno = saved_errno;
-}
-
-
-// Returns <sin6_addr> or <sin_addr> depending on <sa>
-void * get_in_addr(struct sockaddr * sa)
-{
-    if (sa->sa_family == AF_INET)
-    {
-        return &(((struct sockaddr_in *)sa)->sin_addr);
-    }
-    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
-}
-
-// Returns sockfd that server is listening on
-int setup_server ()
-{
-    struct addrinfo hints {0};
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    // Get linked list of address info
-    struct addrinfo * gai_server_info;
-    const char * port {"3490"};
-    int gai_return_value {getaddrinfo(nullptr, port, &hints, &gai_server_info)};
-    if (gai_return_value != 0)
-    {
-        throw gai_strerror(gai_return_value);
-    }
-
-    // Loop through all the <gai_server_info> results and bind to the first we can
-    int yes {1};
-    int listen_sockfd;
-    struct addrinfo * current_ai;
-    for(current_ai = gai_server_info; current_ai != nullptr; current_ai = current_ai->ai_next)
-    {
-        listen_sockfd = socket(current_ai->ai_family, current_ai->ai_socktype, current_ai->ai_protocol);
-        if ( listen_sockfd == -1)
+    public:
+        Stash_Server(int port = 3490) : m_acceptor (m_io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), m_port)) // Create acceptor for any ipv4
         {
-            std::perror("stash_server - socket");
-            continue;
+            m_port = port;
         }
 
-        if (setsockopt(listen_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        void send(socket, std::string message)
         {
-            std::perror("stash_server - setsockopt");
-            throw "stash_server: setsockopt operation failed in setup_server";
+            boost::system::error_code ignored_error;
+            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
         }
 
-        if (bind(listen_sockfd, current_ai->ai_addr, current_ai->ai_addrlen) == -1)
+        void run()
         {
-            close(listen_sockfd);
-            std::perror("stash_server - bind");
-            continue;
+            boost::asio::ip::tcp::socket socket(m_io_context);
+
+            for(;;)
+            {
+                m_acceptor.accept(socket); // Accept connection to socket
+                send(socket, "Hello there");
+            }
         }
-
-        break;
-    }
-
-    freeaddrinfo(gai_server_info);
-
-    if (current_ai == nullptr)
-    {
-        throw "stash_server: bind operation failed in setup_server";
-    }
-
-    int backlog {10};
-    if (listen(listen_sockfd, backlog) == -1)
-    {
-        std::perror("stash_server - listen");
-        throw "stash_server: listen operation failed in setup_server";
-    }
-
-    return listen_sockfd;
-}
-
-// Outputs connection info and returns client's socket file descriptor
-int accept_client(const int listen_sockfd)
-{
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size {sizeof their_addr};
-    int client_sockfd {accept(listen_sockfd, (sockaddr*)(&their_addr), &sin_size)};
-    if (client_sockfd == -1)
-    {
-        return -1;
-    }
-
-    char ip_string[INET6_ADDRSTRLEN];
-    inet_ntop(their_addr.ss_family, get_in_addr((sockaddr *)(&their_addr)), ip_string, sizeof ip_string);
-    std::cout << "stash_server: got connection from " << ip_string << "\n";
-    return client_sockfd;
-}
-
-// Attempts send operation <max_attempts> times to <client_sockfd>
-void send_file (const int client_sockfd)
-{
-    int attempt_num = 0;
-    constexpr int max_attempts {10};
-    while (attempt_num < max_attempts)
-    {
-        long int send_result {send(client_sockfd, "Hello, world!", 13, 0)};
-        if (send_result == -1)
-        {
-            std::cerr << "stash_server - send attempt failed (" << attempt_num << " of " << max_attempts <<")\n";
-            std::perror("");
-            ++attempt_num;
-            continue;
-        }
-        break;
-    }
-    if (attempt_num < max_attempts)
-    {
-        std::cout << "stash_server: successful send\n";
-    }
-    else
-    {
-        std::cout << "stash_server: unsuccessful send\n";
-    }
-}
+};
 
 int main()
 {
-    try
-    {
-        int listen_sockfd {setup_server()};
+  try
+  {
+    Stash_Server server;
+    server.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
 
-        // Handle child process termination signal
-        struct sigaction sa;
-        sa.sa_handler = sigchld_handler;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = SA_RESTART;
-        if (sigaction(SIGCHLD, &sa, nullptr) == -1)
-        {
-            std::perror("stash_server - sigaction");
-            throw "stash_server: sigaction failed";
-        }
-
-        // Accept loop
-        std::cout << "stash_server: waiting for connections...\n";
-        while(true)
-        {
-            int client_sockfd {accept_client(listen_sockfd)};
-            if (client_sockfd == -1)
-            {
-                continue;
-            }
-            pid_t fork_pid {fork()};
-            switch(fork_pid)
-            {
-                case -1: // Fork failed
-                    std::cerr << "stash_server: Failed to fork\n";
-                    break;
-                case 0: // Child
-                    close(listen_sockfd);
-                    send_file(client_sockfd);
-                    close(client_sockfd);
-                    exit(0);
-                default: // Parent
-                    break;
-            }
-            close(client_sockfd);
-        }
-        close(listen_sockfd);
-    }
-    catch(...)
-    {
-        std::cout << "stash_server: an unexpected exception occurred.\n";
-    }
-    return 0;
+  return 0;
 }
+
