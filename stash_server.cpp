2abfeeed0be83a750d4f0dc79dfa9e3c2f09767f
rab <r.ahmedbuehler@gmail.com>
2021-05-18 21:23:38 -0700

    Add session class

--- stash_server.cpp
+++ stash_server.cpp
@@ -5,13 +5,19 @@
 
 // g++ -I ~/HomeExt/boost_1_76_0 stash_server.cpp -o server.out -std=c++17 -pthread
 
-class Stash_Server
+class Stash_Session
 {
     private:
-        int m_port;
-        boost::asio::ip::tcp::acceptor m_acceptor;
-        bool m_files_stored;
+        boost::asio::ip::tcp::socket m_session_sock;
         std::vector <std::vector <std::byte>> m_storage;
+
+        bool files_stored()
+        {
+            if (m_storage.size() == 0)
+                return false;
+            else
+                return true;
+        }
 
         std::vector <std::string> parse_first_request (std::vector<char> data)
         {
@@ -44,6 +50,7 @@ class Stash_Server
 
         void push_files()
         {
+            m_storage.clear();
         }
 
         void pull_files()
@@ -51,11 +58,58 @@ class Stash_Server
         }
 
     public:
+        Stash_Session(boost::asio::ip::tcp::socket& session_sock, std::vector <std::vector <std::byte>>& storage)
+        {
+            // This vs passing pointer?
+            m_session_sock = session_sock;
+            m_storage = storage;
+        }
+
+        void start()
+        {
+            std::vector <char> data (128);
+            boost::asio::read(m_session_sock, data)
+            std::vector <std::string> args {parse_first_request(data)};
+
+            switch(args[0])
+            {
+                case "push":
+                    if (files_stored())
+                    {
+                        boost::asio::write(m_session_sock, boost::asio::buffer("!"));
+                    }
+                    push_files(m_session_sock);
+                    break;
+                case "pull":
+                    if (not files_stored())
+                    {
+                        boost::asio::write(m_session_sock, boost::asio::buffer("!"));
+                        break;
+                    }
+                    pull_files(m_session_sock);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+
+class Stash_Server
+{
+    private:
+        int m_port;
+        // This eventually needs to be generalized for more than one user
+        std::vector <std::vector <std::byte>> m_storage;
+        boost::asio::ip::tcp::acceptor m_acceptor;
+
+
+    public:
         Stash_Server(boost::asio::io_context& io_context, int port = 3490)
             // Create acceptor for any ipv4 endpoint; opens, binds, and listens on endpoint
             : m_port{port}, m_acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), m_port))
         {
-            m_files_stored = false;
         }
 
         void run()
@@ -64,38 +118,19 @@ class Stash_Server
             {
                 try
                 {
-                    boost::asio::ip::tcp::socket server_socket(io_context);
-                    m_acceptor.accept(server_socket); // Accept connection to socket
 
-                    std::vector <char> data (128);
-                    boost::asio::read(server_socket, data)
-                    std::vector <std::string> args {parse_first_request(data)};
+                    // Create and accept connection to socket
+                    boost::asio::ip::tcp::socket session_socket(io_context);
+                    m_acceptor.accept(session_socket);
 
-                    switch(args[0])
-                    {
-                        case "push":
-                            if (m_files_stored)
-                            {
-                                boost::asio::write(server_socket, boost::asio::buffer("!"));
-                            }
-                            push_files(server_socket);
-                            break;
-                        case "pull":
-                            if (not m_files_stored)
-                            {
-                                boost::asio::write(server_socket, boost::asio::buffer("!"));
-                                break;
-                            }
-                            pull_files(server_socket);
-                            break;
-                        default:
-                            break;
-                    }
 
-                    // Close <server_socket>
-                    server_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-                    server_socket.close();
+                    // Handle current session
+                    Stash_Session session (session_socket, m_storage);
+                    session.run();
 
+                    // Close socket
+                    session_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+                    session_socket.close();
                 }
                 catch (boost::system::system_error &e)
                 {
